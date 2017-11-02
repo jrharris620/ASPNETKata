0794b9ac382fbfd116ce681fdb57c6d598519db1
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-11-02 14:36:14 -0500

    Created Product Class, Controller, and View. Also imported info using Dapper from MySQL

---
+++ ASPNETKata/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETKata.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public double ListPrice { get; set; }
+        public bool MakeFlag { get; set; }
+    }
+}
